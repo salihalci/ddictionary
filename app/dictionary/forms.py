3d5f70b6a5ce5a85bad1ba875633aa55103a0117
salci <salih.alci@gmail.com>
2024-12-27 14:04:06 +0300

    updated labels of save form

--- app/dictionary/forms.py
+++ app/dictionary/forms.py
@@ -5,11 +5,15 @@ from .models import Word
 
 class WordSaveUpdate(forms.ModelForm):
 
-    word_en = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'Turkish' ,
+    word_en = forms.CharField(
+        label="Turkish",
+        widget=forms.TextInput(
+        attrs={'placeholder': 'Turkish word...' ,
                'class':'form-control'}))
-    translation_tr = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'English' ,
+    translation_tr = forms.CharField(
+        label="English",
+        widget=forms.TextInput(
+        attrs={'placeholder': 'English word...' ,
                'class':'form-control'}))
 
     class Meta:
