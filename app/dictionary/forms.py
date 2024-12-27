0bd20de1f6824133f718411b14622f533e22dc4f
salci <salih.alci@gmail.com>
2024-12-27 13:31:44 +0300

    updated layouts.

--- app/dictionary/forms.py
+++ app/dictionary/forms.py
@@ -6,10 +6,14 @@ from .models import Word
 class WordSaveUpdate(forms.ModelForm):
 
     word_en = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'Turkis'}))
+        attrs={'placeholder': 'Turkish' ,
+               'class':'form-control'}))
     translation_tr = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'English'}))
+        attrs={'placeholder': 'English' ,
+               'class':'form-control'}))
 
     class Meta:
         model = Word
         fields = ["word_en", "translation_tr"]
+
+    
